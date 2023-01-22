1d1941bec6858a786a227d0f0d2ba7f9e4855c7b
Kamzin <olegkamzin@gmail.com>
2023-01-22 12:36:27 +0300

    Новый тестовый проект

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="km">
+		<h1>Отправка маркировок</h1>
+		<input class="input_km" type="text" v-model="km" @change=sendKm(km) />
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+	name: 'HomeView',
+	data() {
+		return {
+			km: ''
+		}
+	},
+	methods: {
+		async sendKm(km) {
+			await axios.post('https://vorstack.ru:8087/km', {
+				km: km
+			})
+			this.km = ''
+		}
+	}
 }
 </script>
+
+<style>
+.km {
+	padding: 40px;
+}
+.input_km {
+	width: 100%;
+	margin: 20px 0 0 0;
+	padding: 10px 20px;
+	border: 2px solid rgb(247, 218, 0);
+	border-radius: 7px;
+	outline: none;
+}
+.input_km:focus {
+	border: 2px solid rgb(247, 0, 0);
+}
+</style>
